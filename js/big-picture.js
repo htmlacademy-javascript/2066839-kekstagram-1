1825fc04d530343a1bf26a9a64724a2b431db111
Vladimir <salazar888@mail.ru>
2024-03-30 16:47:39 +0300

    Работа над ошибками 8ч.1

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,20 +1,15 @@
-/* eslint-disable no-use-before-define */
-const SHOW_BY_5 = 5;
-
 const bigPicture = document.querySelector('.big-picture');
-const body = document.querySelector('body');
 const commentsCount = document.querySelector('.social__comment-count');
 const commentsLoader = document.querySelector('.comments-loader');
 const commentsList = document.querySelector('.social__comments');
 const bigPictureCancel = document.querySelector('.big-picture__cancel');
-
-let comments = [];
-let commentsShows = 0;
+const socialCommentTemplate = document.querySelector('#social-comment')
+  .content
+  .querySelector('.social__comment');
 
 const createComment = ({ avatar, name, message }) => {
-  const comment = document.createElement('li');
-  comment.innerHTML = '<img class="social__picture" src="" alt="" width="35" height="35">';
-  comment.insertAdjacentHTML('beforeend', '<p class="social__text"></p>');
+  const comment = socialCommentTemplate.cloneNode(true);
+
   comment.querySelector('.social__text').textContent = message;
   comment.querySelector('.social__picture').src = avatar;
   comment.querySelector('.social__picture').alt = name;
@@ -30,55 +25,45 @@ const renderPhotoDetails = ({ url, likes, description }) => {
   bigPicture.querySelector('.social__caption').textContent = description;
 };
 
-const renderComments = () => {
-  commentsShows += SHOW_BY_5;
-
-  if (commentsShows >= comments.length) {
-    commentsLoader.classList.add('hidden');
-    commentsShows = comments.length;
-  } else {
-    commentsLoader.classList.remove('hidden');
-  }
+const renderComments = (comments) => {
 
   const fragment = document.createDocumentFragment();
-  for (let i = 0; i < commentsShows; i++) {
-    const commentElement = createComment(comments[i]);
+
+  comments.forEach((comment) => {
+    const commentElement = createComment(comment);
     fragment.append(commentElement);
-  }
+  });
 
   commentsList.innerHTML = '';
-  commentsCount.innerHTML = `${commentsShows} из <span class="comments-count">${comments.length}</span> комментариев`;
+  commentsCount.innerHTML = `${comments} из <span class="comments-count">${comments.length}</span> комментариев`;
   commentsList.append(fragment);
 };
 
 export const showBigPicture = (data) => {
   bigPicture.classList.remove('hidden');
-  body.classList.add('.modal-open');
+  document.body.classList.add('.modal-open');
+  commentsLoader.classList.add('hidden');
+  commentsCount.classList.add('hidden');
   document.addEventListener('keydown', onDocumentKeydown);
 
   renderPhotoDetails(data);
-  comments = data.comments;
-  if (comments.length > 0) {
-    renderComments();
-  }
+  renderComments(data.comments);
 };
 
-const onDocumentKeydown = (evt) => {
+function onDocumentKeydown (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
     hideBigPicture();
   }
-};
+}
 
-const hideBigPicture = () => {
+function hideBigPicture () {
   bigPicture.classList.add('hidden');
-  body.classList.remove('.modal-open');
+  document.body.classList.remove('.modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-  commentsShows = 0;
-};
+}
 
 const onCancelButtonClick = () => hideBigPicture();
-const onCommentsLoaderClick = () => renderComments();
 
 bigPictureCancel.addEventListener('click', onCancelButtonClick);
-commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,4 @@
-import { renderThumbnails } from './render-thumbnails.js';
+import { renderThumbnail } from './render-thumbnails.js';
 import { showBigPicture } from './big-picture.js';
 
 const container = document.querySelector('.pictures');
@@ -6,14 +6,20 @@ const container = document.querySelector('.pictures');
 export const renderGallery = (pictures) => {
   container.addEventListener('click', (evt) => {
     const thumbnail = evt.target.closest('[data-thumbnail-id]');
+
     if (!thumbnail) {
       return;
     }
 
     const picture = pictures.find((item) => item.id === +thumbnail.dataset.thumbnailId);
+
+    if (!picture) {
+      return;
+    }
+
     showBigPicture(picture);
   });
 
-  renderThumbnails(pictures, container);
+  renderThumbnail(pictures);
 };
 
