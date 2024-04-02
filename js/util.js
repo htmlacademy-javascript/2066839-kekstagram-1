4be665e0aca18ad26ad826e8cc06fa6f51d27fd6
Vladimir <salazar888@mail.ru>
2024-04-02 19:41:39 +0300

    !

---
+++ js/util.js
@@ -0,0 +1,10 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomElement = (array) => array[getRandomInteger(0, array.length - 1)];
+
+export { getRandomElement, getRandomInteger };
