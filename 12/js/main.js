5b17cb5915e5584375fd56e2b597b48ad2328d40
Keks <keks@htmlacademy.ru>
2024-04-17 12:29:21 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,7 @@
+import { createGallery } from './render-comments.js';
+import { renderGallery } from './gallery.js';
+import './image-edit-form.js';
+import './scale.js';
+import './effects.js';
+
+renderGallery(createGallery());
---
+++ 12/js/scale.js
@@ -0,0 +1,33 @@
+const SCALE_STEP = 25;
+const MAX_SCALE = 100;
+const MIN_SCALE = 25;
+const SCALE_DEFAULT = 100;
+
+const scaleUpButton = document.querySelector('.scale__control--bigger');
+const scaleDownButton = document.querySelector('.scale__control--smaller');
+const scaleInputEl = document.querySelector('.scale__control--value');
+const imageEl = document.querySelector('.img-upload__preview img');
+
+const scaleImage = (value) => {
+  imageEl.style.transform = `scale(${value / 100})`;
+  scaleInputEl.value = `${value}%`;
+};
+
+const getValue = () => parseInt(scaleInputEl.value, 10);
+
+const onClickButtonBigger = () => {
+  const newValue = Math.min(getValue() + SCALE_STEP, MAX_SCALE);
+
+  scaleImage(newValue);
+};
+
+const onClickButtonSmaller = () => {
+  const newValue = Math.max(getValue() - SCALE_STEP, MIN_SCALE);
+
+  scaleImage(newValue);
+};
+
+export const resetScaleValue = () => scaleImage(SCALE_DEFAULT);
+
+scaleUpButton.addEventListener('click', onClickButtonBigger);
+scaleDownButton.addEventListener('click', onClickButtonSmaller);
